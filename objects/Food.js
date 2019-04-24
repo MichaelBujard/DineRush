688316901939f0bd3f7934c254145af1c377e3ba
rayanazzam <rahmed@udallas.edu>
2019-04-24 16:47:34 -0500

    added a few object templates for app. Need public setters and getters still

---
+++ objects/Food.js
@@ -0,0 +1,16 @@
+/*
+Food object has the following properties:
+(Java fields for simplicity)
+private String name, imgSrc, description, menuId, price, inStock;
+*/
+
+class Food {
+    contructor() {
+        this.name = "none"
+        this.imgSrc = "none" // http link to an image
+        this.description = "none"
+        this.menuId = "none"
+        this.price = "none"
+        this.inStock = "none"
+    }
+}
---
+++ objects/Requests.js
@@ -0,0 +1,16 @@
+class Requests {
+    /*
+
+    Request objects has the following fields:
+    (Java fields for simplicity)
+    private String username, fullName, address, total;
+    private List<Order> foods;
+    */
+    constructor() {
+        this.username = "none"
+        this.fullname = "none"
+        this.address = "none"
+        this.total = "noe";
+        this.foods = "none"
+    }
+}
